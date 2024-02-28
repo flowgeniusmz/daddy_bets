ef92e544327e0eeae1839b238f91e011a524c158
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-02-27 22:56:43 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,8 @@ from config import pagesetup as ps
 from openai import OpenAI
 import time
 from app import assistant_chat as asst, background_image as bckim, betting_form as bf
+with open( "config\style.css" ) as css:
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
 # 1. Set Page Config
 app_name = "Daddy Bets"
