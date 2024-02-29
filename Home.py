bcaf8d544ae6a45553e8c295ca88474cbfc82ecd
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-02-29 15:26:30 +0000

    s

--- Home.py
+++ Home.py
@@ -1,23 +1,5 @@
 import streamlit as st
-from config import pagesetup as ps
-from openai import OpenAI
-import time
-from app import assistant_chat as asst, background_image as bckim, betting_form as bf
-with open( "config/style.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
-
-# 1. Set Page Config
-app_name = "Daddy Bets"
-page_title = "Daddy Bets"
-page_subtitle = "Betting Assistant" 
-page_icon = "https://storage.googleapis.com/production-domaincom-v1-0-8/048/1724048/4RBifvGs/dfc737c8f0d640cfa7e8623583bfcf5e"
-page_description = "Is a gambler's best friend. It is capable of taking your Odds Assistant odds and analyzing the smart bets for you."
-overview_header = "Overview"
-overview_text = f"**{page_subtitle}** {page_description.lower()}"
-#st.set_page_config(page_icon=page_icon, layout="wide")
-
 from config import pagesetup as ps, sessionstates as ss
-
 
 
 # 0. Page Config
@@ -25,12 +7,6 @@ ps.get_st_page_config()
 
 with open( "config/style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
-
-
-# 4. Set assistant chat
-bckim.display_background_image()
-bf.display_betting_form()
-asst.app_chat()
 
 # 1. Page Title
 ps.get_title(0)
@@ -54,4 +30,3 @@ with link_container:
     with link_columns[1]:
         ps.get_page_link(2)
         ps.get_page_link(4)
-
